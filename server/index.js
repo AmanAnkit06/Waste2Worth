63e0e2e93ce2a0c7e20ca629fbd63045db0ad4ff
Ashish <ashishgupta2972002@gmail.com>
2025-04-18 20:06:53 +0530

    added backend url

--- server/index.js
+++ server/index.js
@@ -27,7 +27,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
 	cors({
-		origin: "http://localhost:3000",
+		origin: "https://waste2worth-puce.vercel.app",
 		credentials: true,
 	})
 );
